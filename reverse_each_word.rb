4b9dde261898f7ddcfe30b090a2894fabc6d1e8e
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-29 01:56:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 def reverse_each_word(sentence1)
   
+  #first method
   new_array = sentence1.split(" ")
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
- 
+  
+  #second method
+  sentence1.split(" ").collect {|x| x.reverse!}.join(" ")
+  
 end
